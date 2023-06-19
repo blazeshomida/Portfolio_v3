d61da3c0bbc912ee607647a72173c019f232bbca
Blaze S <131521446+blazeshomida@users.noreply.github.com>
2023-06-19 00:39:53 -1000

    Form connection

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -3,7 +3,7 @@ import styles from './index.module.scss'
 
 const Form = ({children}) => {
   return (
-    <form action="https://formsubmit.co/4f5b704d4af3f6f6a2cf255f843f2ddb" method="POST" className={styles['form']} >
+    <form action="https://formsubmit.co/bshomida@gmail.com" method="POST" className={styles['form']} >
       <input type="hidden" name="_subject" value="New form submission!" />
       {children}
       </form>
